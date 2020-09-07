ec7e4ea57e2ab6220abd095dc8e22c62606d6279
xwp <xwp_fullstack@163.com>
2020-09-07 08:14:19 +0800

    项目配置

--- deiduo/deiduo/dev/settings.py
+++ deiduo/deiduo/dev/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'deiduo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'deiduo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD':"mysql",
+        # 'HOST': '192.168.6.130',
+        'HOST':'127.0.0.1',
+        'PORT': 3306,
+        'NAME': "deiduo",
     }
 }
 
@@ -118,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
