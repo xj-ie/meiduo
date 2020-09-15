456082199bcdd3ea3f8dbaef05dc40a4056fd89c
ie <273817691@qq.com>
2020-09-15 21:54:45 +0800

    register_show

--- deiduo/deiduo/apps/register/views.py
+++ deiduo/deiduo/apps/register/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views import View
+from rest_framework.views import APIView
 # Create your views here.
+
+class RegisterView(View):
+
+    def get(self,request):
+        return render(request, 'register.html')
+    def post(self,request):
+        pass
